9f76df839bf3b936a54dc57a55e29325f7986600
Vivek Shrivastava <vivek.shrivastava2005@gmail.com>
2025-08-18 00:21:11 +0530

    daily checkin , posted answer too

--- Programs/Python/LeetCode/Easy/1323_Max69Number.py
+++ Programs/Python/LeetCode/Easy/1323_Max69Number.py
@@ -1,3 +1,19 @@
+
+#https://leetcode.com/problems/maximum-69-number
+#Intuition : Just change 6 to 9 starting from left to right , return same number if no change
+#Make sure to typecast to int
+#Beats 100%
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        
+        max = num 
+        max = list(str(max))
+        num = list(str(num))
+        for i in range(len(num)):
+            if num[i]=='6':
+                max[i]='9'
+                return int(''.join(max))
+        return int(''.join(num))
+
+sol = Solution()
+print(sol.maximum69Number(9696))
+
